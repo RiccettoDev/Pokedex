f26bb4c50417a272dcd6b964f818ec5e8058fbc1
Eduardo <princesherlockk@gmail.com>
2023-12-04 12:02:36 -0300

    [Pokedex]- implemented button prev and next

--- src/js/script.js
+++ src/js/script.js
@@ -4,32 +4,59 @@ const pokemonImage = document.querySelector('.pokemon__image')
 
 const form = document.querySelector('.form')
 const input = document.querySelector('.input__search')
+const buttonPrev = document.querySelector('.btn-prev')
+const buttonNext = document.querySelector('.btn-next')
+
+let searchPokemon = 1
 
 const fetchPokemon = async pokemon => {
   const APIResponse = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`
+    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
   )
 
-  const data = await APIResponse.json()
-
-  return data
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json()
+    return data
+  }
 }
 
 const renderPokemon = async pokemon => {
+  pokemonName.innerHTML = 'Loading...'
+
   const data = await fetchPokemon(pokemon)
 
-  pokemonName.innerHTML = data.name
-  pokemonNumber.innerHTML = data.id
-  pokemonImage.src =
-    data['sprites']['versions']['generation-v']['black-white']['animated'][
-      'front_default'
-    ]
+  if (data) {
+    pokemonName.innerHTML = data.name
+    pokemonNumber.innerHTML = data.id
+    pokemonImage.src =
+      data['sprites']['versions']['generation-v']['black-white']['animated'][
+        'front_default'
+      ]
 
-  input.value = ''
+    input.value = ''
+    searchPokemon = data.id
+  } else {
+    pokemonName.innerHTML = 'Not found :('
+    pokemonNumber.innerHTML = ''
+    pokemonImage.src = ''
+    input.value = ''
+  }
 }
 
 form.addEventListener('submit', event => {
   event.preventDefault()
 
-  renderPokemon(input.value)
+  renderPokemon(input.value.toLowerCase())
 })
+
+buttonPrev.addEventListener('click', () => {
+  searchPokemon -= 1
+  renderPokemon(searchPokemon)
+})
+
+buttonNext.addEventListener('click', () => {
+  searchPokemon += 1
+  renderPokemon(searchPokemon)
+})
+
+renderPokemon(searchPokemon)
